c707ab9445f95028a6e5b8aba873a0c140d2e72f
Shay Rojansky <roji@roji.org>
2023-10-13 10:12:40 +0200

    Add wrappers for the Points APIs

--- src/Qdrant.Client/LoggingExtensions.cs
+++ src/Qdrant.Client/LoggingExtensions.cs
@@ -4,37 +4,104 @@ namespace Qdrant.Client;
 
 internal static partial class LoggingExtensions
 {
-	[LoggerMessage(1, LogLevel.Debug, "Create collection '{collection}'")]
+	[LoggerMessage(1000, LogLevel.Debug, "Create collection '{collection}'")]
 	public static partial void CreateCollection(this ILogger logger, string collection);
 
-	[LoggerMessage(2, LogLevel.Debug, "Get collection info for '{collection}'")]
+	[LoggerMessage(1001, LogLevel.Debug, "Get collection info for '{collection}'")]
 	public static partial void GetCollectionInfo(this ILogger logger, string collection);
 
-	[LoggerMessage(3, LogLevel.Debug, "List collections")]
+	[LoggerMessage(1003, LogLevel.Debug, "List collections")]
 	public static partial void ListCollections(this ILogger logger);
 
-	[LoggerMessage(4, LogLevel.Debug, "Delete collection '{collection}'")]
+	[LoggerMessage(1004, LogLevel.Debug, "Delete collection '{collection}'")]
 	public static partial void DeleteCollection(this ILogger logger, string collection);
 
-	[LoggerMessage(5, LogLevel.Debug, "Update collection '{collection}'")]
+	[LoggerMessage(1005, LogLevel.Debug, "Update collection '{collection}'")]
 	public static partial void UpdateCollection(this ILogger logger, string collection);
 
 	#region Alias management
 
-	[LoggerMessage(1000, LogLevel.Debug, "Create alias '{alias}' for collection '{collection}'")]
+	[LoggerMessage(2000, LogLevel.Debug, "Create alias '{alias}' for collection '{collection}'")]
 	public static partial void CreateAlias(this ILogger logger, string alias, string collection);
 
-	[LoggerMessage(1001, LogLevel.Debug, "Delete alias '{alias}'")]
+	[LoggerMessage(2001, LogLevel.Debug, "Delete alias '{alias}'")]
 	public static partial void DeleteAlias(this ILogger logger, string alias);
 
-	[LoggerMessage(1002, LogLevel.Debug, "Rename alias '{oldAlias}' to '{newAlias}'")]
+	[LoggerMessage(2002, LogLevel.Debug, "Rename alias '{oldAlias}' to '{newAlias}'")]
 	public static partial void RenameAlias(this ILogger logger, string oldAlias, string newAlias);
 
-	[LoggerMessage(1003, LogLevel.Debug, "List aliases for collection '{collection}'")]
+	[LoggerMessage(2003, LogLevel.Debug, "List aliases for collection '{collection}'")]
 	public static partial void ListCollectionAliases(this ILogger logger, string collection);
 
-	[LoggerMessage(1004, LogLevel.Debug, "List all aliases")]
+	[LoggerMessage(2004, LogLevel.Debug, "List all aliases")]
 	public static partial void ListAliases(this ILogger logger);
 
 	#endregion Alias management
+
+	#region Point management
+
+	[LoggerMessage(3004, LogLevel.Debug, "Upsert {count} points into '{collection}'")]
+	public static partial void Upsert(this ILogger logger, string collection, int count);
+
+	[LoggerMessage(3005, LogLevel.Debug, "Delete from '{collection}'")]
+	public static partial void Delete(this ILogger logger, string collection);
+
+	[LoggerMessage(3006, LogLevel.Debug, "Retrieve points from '{collection}'")]
+	public static partial void Retrieve(this ILogger logger, string collection);
+
+	[LoggerMessage(3007, LogLevel.Debug, "Update vectors in '{collection}'")]
+	public static partial void UpdateVectors(this ILogger logger, string collection);
+
+	[LoggerMessage(3008, LogLevel.Debug, "Delete vectors in '{collection}'")]
+	public static partial void DeleteVectors(this ILogger logger, string collection);
+
+	[LoggerMessage(3009, LogLevel.Debug, "Set payload in '{collection}'")]
+	public static partial void SetPayload(this ILogger logger, string collection);
+
+	[LoggerMessage(3010, LogLevel.Debug, "Overwrite payload in '{collection}'")]
+	public static partial void OverwritePayload(this ILogger logger, string collection);
+
+	[LoggerMessage(3011, LogLevel.Debug, "Delete payload in '{collection}'")]
+	public static partial void DeletePayload(this ILogger logger, string collection);
+
+	[LoggerMessage(3012, LogLevel.Debug, "Clear payload in '{collection}'")]
+	public static partial void ClearPayload(this ILogger logger, string collection);
+
+	[LoggerMessage(3013, LogLevel.Debug, "Create payload field index in '{collection}'")]
+	public static partial void CreatePayloadIndex(this ILogger logger, string collection);
+
+	[LoggerMessage(3014, LogLevel.Debug, "Delete payload field index in '{collection}'")]
+	public static partial void DeletePayloadIndex(this ILogger logger, string collection);
+
+	[LoggerMessage(3015, LogLevel.Debug, "Search on '{collection}'")]
+	public static partial void Search(this ILogger logger, string collection);
+
+	[LoggerMessage(3016, LogLevel.Debug, "Search batch on '{collection}'")]
+	public static partial void SearchBatch(this ILogger logger, string collection);
+
+	[LoggerMessage(3017, LogLevel.Debug, "Search groups on '{collection}'")]
+	public static partial void SearchGroups(this ILogger logger, string collection);
+
+	[LoggerMessage(3018, LogLevel.Debug, "Scroll on '{collection}'")]
+	public static partial void Scroll(this ILogger logger, string collection);
+
+	[LoggerMessage(3019, LogLevel.Debug, "Recommend on '{collection}'")]
+	public static partial void Recommend(this ILogger logger, string collection);
+
+	[LoggerMessage(3020, LogLevel.Debug, "Recommend batch on '{collection}'")]
+	public static partial void RecommendBatch(this ILogger logger, string collection);
+
+	[LoggerMessage(3021, LogLevel.Debug, "Recommend groups on '{collection}'")]
+	public static partial void RecommendGroups(this ILogger logger, string collection);
+
+	[LoggerMessage(3022, LogLevel.Debug, "Update batch on '{collection}'")]
+	public static partial void UpdateBatch(this ILogger logger, string collection);
+
+	[LoggerMessage(3023, LogLevel.Debug, "Count points in '{collection}'")]
+	public static partial void Count(this ILogger logger, string collection);
+
+	#endregion Point management
+
+	[LoggerMessage(99999, LogLevel.Error, "Operation failed: {operation}")]
+	public static partial void OperationFailed(this ILogger logger, string operation, Exception exception);
 }
