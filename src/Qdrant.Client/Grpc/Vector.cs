73f90240898a3db465fb7db54ced3fd4a98809a0
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-12-08 10:32:56 +0100

    Update src/Qdrant.Client/Grpc/Vector.cs
    Co-authored-by: Russ Cam <russ.cam@forloop.co.uk>

--- src/Qdrant.Client/Grpc/Vector.cs
+++ src/Qdrant.Client/Grpc/Vector.cs
@@ -13,25 +13,25 @@ public partial class Vector
 	public static implicit operator Vector(float[] values) => new() { Data = { values } };
 
 	/// <summary>
-	/// Implicitly converts an tuple of sparse values array of <see cref="float"/> 
+	/// Implicitly converts a tuple of sparse values array of <see cref="float"/>
 	/// and sparse indices array of <see cref="uint"/> to a new instance of <see cref="Vector"/>
 	/// </summary>
-	/// <param name="sparse_values">list of values and indices</param>
+	/// <param name="sparseValues">a tuple of arrays of values and indices</param>
 	/// <returns>a new instance of <see cref="Vector"/></returns>
-	public static implicit operator Vector((float[], uint[]) sparse_values) => new()
+	public static implicit operator Vector((float[], uint[]) sparseValues) => new()
 	{
-		Data = { sparse_values.Item1 },
-		Indices = new SparseIndices { Data = { sparse_values.Item2 } }
+		Data = { sparseValues.Item1 },
+		Indices = new SparseIndices { Data = { sparseValues.Item2 } }
 	};
 
 	/// <summary>
-	/// Implicitly converts an array of sparse vector value-index pairs to a new instance of <see cref="Vector"/>
+	/// Implicitly converts an array of sparse value index tuples to a new instance of <see cref="Vector"/>
 	/// </summary>
-	/// <param name="sparse_values">the array of value-index pairs</param>
+	/// <param name="sparseValues">the array of value-index pairs</param>
 	/// <returns>a new instance of <see cref="Vector"/></returns>
-	public static implicit operator Vector((float, uint)[] sparse_values) => new()
+	public static implicit operator Vector((float, uint)[] sparseValues) => new()
 	{
-		Data = { sparse_values.Select(v => v.Item1) },
-		Indices = new SparseIndices { Data = { sparse_values.Select(v => v.Item2) } }
+		Data = { sparseValues.Select(v => v.Item1) },
+		Indices = new SparseIndices { Data = { sparseValues.Select(v => v.Item2) } }
 	};
 }
