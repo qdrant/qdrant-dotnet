1aa2da4af0f3b9d9aba01a0a93539c38f9dadcdf
Anush008 <anushshetty90@gmail.com>
2024-02-16 23:52:44 +0530

    chore: updated convinience methods

--- src/Qdrant.Client/Grpc/WithPayloadSelector.cs
+++ src/Qdrant.Client/Grpc/WithPayloadSelector.cs
@@ -11,4 +11,39 @@ public partial class WithPayloadSelector
 	/// <param name="enable">If <code>true</code> return all payload, if <code>false</code> then none</param>
 	/// <returns>a new instance of <see cref="WithPayloadSelector"/></returns>
 	public static implicit operator WithPayloadSelector(bool enable) => new() { Enable = enable };
+
+	/// <summary>
+	/// Implicitly converts <see cref="string"/> to a new instance of <see cref="WithPayloadSelector"/>
+	/// </summary>
+	/// <param name="fields">List of fields in the payload to return.</param>
+	/// <returns>a new instance of <see cref="WithPayloadSelector"/></returns>
+	public static implicit operator WithPayloadSelector(string[] fields) =>
+		new() { Include = new PayloadIncludeSelector { Fields = { fields } } };
+
+	/// <summary>
+	/// Negates the <see cref="WithPayloadSelector"/> condition.
+	/// </summary>
+	/// <param name="selector"><see cref="WithPayloadSelector"/> to negate.</param>
+	/// <returns>a new instance of <see cref="WithPayloadSelector"/></returns>
+	public static WithPayloadSelector operator !(WithPayloadSelector selector)
+	{
+		if (selector.HasEnable)
+		{
+			return new WithPayloadSelector { Enable = !selector.Enable };
+		}
+		else if (selector.Include is not null)
+		{
+			return new WithPayloadSelector
+			{
+				Exclude = new PayloadExcludeSelector { Fields = { selector.Include.Fields } }
+			};
+		}
+		else
+		{
+			return new WithPayloadSelector
+			{
+				Include = new PayloadIncludeSelector { Fields = { selector.Exclude.Fields } }
+			};
+		}
+	}
 }
