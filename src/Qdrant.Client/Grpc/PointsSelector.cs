07bea9863f22f407534d184fdc6f8991e4ffd2cf
Anush008 <anushshetty90@gmail.com>
2024-02-24 21:38:15 +0530

    Review changes

--- src/Qdrant.Client/Grpc/PointsSelector.cs
+++ src/Qdrant.Client/Grpc/PointsSelector.cs
@@ -8,12 +8,14 @@ public partial class PointsSelector
 	/// <summary>
 	/// Implicitly converts a <see cref="Guid" /> ID to a new instance of <see cref="PointsSelector"/>
 	/// </summary>
-	public static implicit operator PointsSelector(Guid id) => new Guid[] { id };
+	public static implicit operator PointsSelector(Guid id) =>
+		new() { Points = new PointsIdsList { Ids = { new PointId { Uuid = id.ToString() } } } };
 
 	/// <summary>
 	/// Implicitly converts a <see cref="ulong" /> ID to a new instance of <see cref="PointsSelector"/>
 	/// </summary>
-	public static implicit operator PointsSelector(ulong id) => new ulong[] { id };
+	public static implicit operator PointsSelector(ulong id) =>
+		new() { Points = new PointsIdsList { Ids = { new PointId { Num = id } } } };
 
 	/// <summary>
 	/// Implicitly converts an array of <see cref="ulong" /> ID to a new instance of <see cref="PointsSelector"/>
--- src/Qdrant.Client/Grpc/WithPayloadSelector.cs
+++ src/Qdrant.Client/Grpc/WithPayloadSelector.cs
@@ -19,31 +19,4 @@ public partial class WithPayloadSelector
 	/// <returns>a new instance of <see cref="WithPayloadSelector"/></returns>
 	public static implicit operator WithPayloadSelector(string[] fields) =>
 		new() { Include = new PayloadIncludeSelector { Fields = { fields } } };
-
-	/// <summary>
-	/// Negates the <see cref="WithPayloadSelector"/> condition.
-	/// </summary>
-	/// <param name="selector"><see cref="WithPayloadSelector"/> to negate.</param>
-	/// <returns>a new instance of <see cref="WithPayloadSelector"/></returns>
-	public static WithPayloadSelector operator !(WithPayloadSelector selector)
-	{
-		if (selector.HasEnable)
-		{
-			return new WithPayloadSelector { Enable = !selector.Enable };
-		}
-		else if (selector.Include is not null)
-		{
-			return new WithPayloadSelector
-			{
-				Exclude = new PayloadExcludeSelector { Fields = { selector.Include.Fields } }
-			};
-		}
-		else
-		{
-			return new WithPayloadSelector
-			{
-				Include = new PayloadIncludeSelector { Fields = { selector.Exclude.Fields } }
-			};
-		}
-	}
 }
