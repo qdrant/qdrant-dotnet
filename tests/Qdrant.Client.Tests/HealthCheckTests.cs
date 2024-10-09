51264d70527cef981680b7249a044e3fee78cf28
Russ Cam <russ.cam@forloop.co.uk>
2024-10-09 16:36:20 +1000

    Remove superfluous semicolon

--- tests/Qdrant.Client.Tests/HealthCheckTests.cs
+++ tests/Qdrant.Client.Tests/HealthCheckTests.cs
@@ -18,14 +18,12 @@ namespace Qdrant.Client
 			response.Title.Should().NotBeNullOrEmpty();
 			response.Version.Should().NotBeNullOrEmpty();
 		}
+
 		[Fact]
 		public async Task Server_Should_Be_UnHealthy()
 		{
 			var client = new QdrantClient("localhost", 9999);
-			;
-
 			await Assert.ThrowsAsync<RpcException>(async () => await client.HealthAsync());
 		}
-
 	}
 }
