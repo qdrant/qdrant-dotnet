553076d37ed7d6ad8b261c625c94cc985c028358
Russ Cam <russ.cam@forloop.co.uk>
2025-03-06 18:17:56 +1000

    Example of mocking high level client
    This commit provides an example of mocking the
    high level client

--- tests/Qdrant.Client.Tests/MockingTests.cs
+++ tests/Qdrant.Client.Tests/MockingTests.cs
@@ -9,19 +9,52 @@ namespace Qdrant.Client;
 public class MockingTests
 {
 	[Fact]
-	public void CanMockClientCalls()
+	public void CanMockGrpcClientCalls()
 	{
 		var client = new Mock<QdrantGrpcClient>();
 		var qdrant = new Mock<Qdrant.Client.Grpc.Qdrant.QdrantClient>();
+
+		var mockResponse = new HealthCheckReply { Title = "from Moq", Version = "v1.0.0" };
 
 		qdrant.Setup(q =>
 				q.HealthCheck(
 					It.IsAny<HealthCheckRequest>(),
 					It.IsAny<CallOptions>()))
-			.Returns(new HealthCheckReply { Title = "from Moq", Version = "v1.0.0" });
+			.Returns(mockResponse);
 
 		client.SetupGet(c => c.Qdrant).Returns(qdrant.Object);
 		var response = client.Object.Qdrant.HealthCheck(new HealthCheckRequest(), new CallOptions());
+
+		response.Should().NotBeNull();
+		response.Title.Should().Be("from Moq");
+		response.Version.Should().Be("v1.0.0");
+	}
+
+	[Fact]
+	public async Task CanMockHighLevelClientCalls()
+	{
+		var grpcClient = new Mock<QdrantGrpcClient>();
+		var client = new QdrantClient(grpcClient.Object);
+		var qdrant = new Mock<Qdrant.Client.Grpc.Qdrant.QdrantClient>();
+
+		var mockResponse = new HealthCheckReply { Title = "from Moq", Version = "v1.0.0" };
+
+		qdrant.Setup(q =>
+				q.HealthCheckAsync(
+					It.IsAny<HealthCheckRequest>(),
+					It.IsAny<Metadata>(),
+					It.IsAny<DateTime?>(),
+					It.IsAny<CancellationToken>()))
+			.Returns(new AsyncUnaryCall<HealthCheckReply>(
+				Task.FromResult(mockResponse),
+				null!,
+				null!,
+				null!,
+				() => { }
+			));
+
+		grpcClient.SetupGet(c => c.Qdrant).Returns(qdrant.Object);
+		var response = await client.HealthAsync();
 
 		response.Should().NotBeNull();
 		response.Title.Should().Be("from Moq");
