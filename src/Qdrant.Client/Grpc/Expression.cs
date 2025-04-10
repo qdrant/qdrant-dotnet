68bf5521a3cb2b3e22d24fbc71e5b6e27da50b0b
Anush008 <anushshetty90@gmail.com>
2025-04-11 01:04:09 +0530

    1.14.0
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

---
+++ src/Qdrant.Client/Grpc/Expression.cs
@@ -0,0 +1,126 @@
+namespace Qdrant.Client.Grpc
+{
+    /// <summary>
+    /// To construct a formula for score boosting.
+    /// </summary>
+    public partial class Expression
+    {
+        /// <summary>
+        /// Implicitly converts a float constant into an <see cref="Expression"/>.
+        /// </summary>
+        public static implicit operator Expression(float constant) =>
+            new() { Constant = constant };
+
+        /// <summary>
+        /// Implicitly converts a variable name (string) into an <see cref="Expression"/>.
+        /// Used to reference a payload key or a score variable.
+        /// </summary>
+        public static implicit operator Expression(string variable) =>
+            new() { Variable = variable };
+
+        /// <summary>
+        /// Implicitly converts a <see cref="Condition"/> into an <see cref="Expression"/>.
+        /// Evaluates to 1.0 if true, 0.0 if false.
+        /// </summary>
+        public static implicit operator Expression(Condition condition) =>
+            new() { Condition = condition };
+
+        /// <summary>
+        /// Implicitly converts a <see cref="GeoDistance"/> into an <see cref="Expression"/>.
+        /// Represents a geographic distance in meters.
+        /// </summary>
+        public static implicit operator Expression(GeoDistance geoDistance) =>
+            new() { GeoDistance = geoDistance };
+
+        /// <summary>
+        /// Converts a date-time constant string into an <see cref="Expression"/>.
+        /// </summary>
+        public static Expression WithDateTimeConstant(string dateTime) =>
+            new() { Datetime = dateTime };
+
+        /// <summary>
+        /// Converts a date-time key string into an <see cref="Expression"/>.
+        /// Used to reference a payload key with date-time values.
+        /// </summary>
+        public static Expression WithDateTimeKey(string dateTimeKey) =>
+            new() { DatetimeKey = dateTimeKey };
+
+        /// <summary>
+        /// Converts a <see cref="MultExpression"/> into an <see cref="Expression"/>.
+        /// </summary>
+        public static implicit operator Expression(MultExpression mult) =>
+            new() { Mult = mult };
+
+        /// <summary>
+        /// Implicitly converts a <see cref="SumExpression"/> into an <see cref="Expression"/>.
+        /// </summary>
+        public static implicit operator Expression(SumExpression sum) =>
+            new() { Sum = sum };
+
+        /// <summary>
+        /// Implicitly converts a <see cref="DivExpression"/> into an <see cref="Expression"/>.
+        /// </summary>
+        public static implicit operator Expression(DivExpression div) =>
+            new() { Div = div };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to a negated expression.
+        /// </summary>
+        public static Expression WithNeg(Expression expr) =>
+            new() { Neg = expr };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to its absolute value.
+        /// </summary>
+        public static Expression WithAbs(Expression expr) =>
+            new() { Abs = expr };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to its square root.
+        /// </summary>
+        public static Expression WithSqrt(Expression expr) =>
+            new() { Sqrt = expr };
+
+        /// <summary>
+        /// Implicitly converts a <see cref="PowExpression"/> to an <see cref="Expression"/>.
+        /// </summary>
+        public static implicit operator Expression(PowExpression pow) =>
+            new() { Pow = pow };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to an exponential.
+        /// </summary>
+        public static Expression WithExp(Expression expr) =>
+            new() { Exp = expr };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to a base-10 logarithm.
+        /// </summary>
+        public static Expression WithLog10(Expression expr) =>
+            new() { Log10 = expr };
+
+        /// <summary>
+        /// Converts an <see cref="Expression"/> to a natural logarithm.
+        /// </summary>
+        public static Expression WithLn(Expression expr) =>
+            new() { Ln = expr };
+
+        /// <summary>
+        /// Converts an exponential decay parameter into an <see cref="Expression"/>.
+        /// </summary>
+        public static Expression WithExpDecay(DecayParamsExpression decay) =>
+            new() { ExpDecay = decay };
+
+        /// <summary>
+        /// Converts a Gaussian decay parameter into an <see cref="Expression"/>.
+        /// </summary>
+        public static Expression WithGaussDecay(DecayParamsExpression decay) =>
+            new() { GaussDecay = decay };
+
+        /// <summary>
+        /// Converts a linear decay parameter into an <see cref="Expression"/>.
+        /// </summary>
+        public static Expression WithLinDecay(DecayParamsExpression decay) =>
+            new() { LinDecay = decay };
+    }
+}
